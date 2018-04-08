ea9cb6053a1e3d7bee496dddd96e1609b9be2bc5
baskervill <basker_ville@163.com>
2018-04-08 09:18:43 +0800

    factory

--- factory.cpp
+++ factory.cpp
@@ -24,6 +24,7 @@ public:
 class Factory{
 public:
     SingleCore* CreateSingleCore(enum CTYPE ctype){
+        //switch
         if(ctype == COREA){
             return new SingleCoreA();
         }
@@ -31,5 +32,44 @@ public:
             return new SingleCoreB();
         }
         else return NULL;
+    }
+};
+
+//factory method
+class SingleCore{
+public:
+    virtual void Show() = 0;
+};
+
+class SingleCoreA : public SingleCore{
+public:
+    void Show(){
+        std::cout<<"SingleCore A"<<std::endl;
+    }
+};
+
+class SingleCoreB : public SingleCore{
+public:
+    void Show(){
+        std::cout<<"SingleCore B"<<std::endl;
+    }
+};
+
+class Factory{
+public:
+    SingleCore* CreateSingleCore() = 0;
+};
+
+class FactoryA : public Factory{
+public:
+    SingleCoreA* CreateSingleCore(){
+        return new SingleCoreA;
+    }
+};
+
+class FactoryB : public Factory{
+public:
+    SingleCoreB* CreateSingleCore(){
+        return new SingleCoreB;
     }
 };
